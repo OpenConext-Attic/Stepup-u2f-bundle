28147e599fd68ede3b378c4ff826af458e6cdec4
Reinier Kip <reinier@ibuildings.nl>
2015-08-31 12:22:41 +0200

    Also include original Yubico U2F error in wrapper LogicException

--- src/Service/SigningService.php
+++ src/Service/SigningService.php
@@ -97,8 +97,9 @@ final class SigningService
                 default:
                     throw new LogicException(
                         sprintf(
-                            'The Yubico U2F service threw an exception with error code %d that should not occur',
-                            $error->getCode()
+                            'The Yubico U2F service threw an exception with error code %d that should not occur (%s)',
+                            $error->getCode(),
+                            $error->getMessage()
                         ),
                         $error
                     );
